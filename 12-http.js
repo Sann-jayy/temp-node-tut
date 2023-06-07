042430c491d2945606ad4f25f29d877cc78f998e
Sanjay Parihar <sanjayparihar888@gmail.com>
2023-06-07 22:48:32 +0530

    first commit

---
+++ 12-http.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if(req.url === '/'){
+        req.end('Welcome to our home page')
+    }
+    if(req.url ==='/about') {
+        res.end('Here is our short history')
+    }
+    res.end(`<h1>Oops!</h1>
+    <p>jdkbnjsnajb</p>
+    <a href = "/">back hokee</a>`)
+})
+
+server.listen(5000)
